0f282f898e1d3bdc6541364b75b3bf3bf98daa52
Ahmad <github email address>
2018-06-26 20:20:21 -0700

    add & populate seed file

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# Add seed data here. Seed your database with `rake db:seed`
+sophie = Owner.create(name: "Sophie")
+legend_of_zelda = Game.create(name: "Legend of Zelda", genre: "role-play", game_console_id: 1)
+donkey_kong = Game.create(name: "Donkey Kong", genre: "action", game_console_id: 1)
+super_mario = Game.create(name: "Super Mario", genre: "role play", game_console_id: 1)
+final_fantasy = Game.create(name: "Final Fantasy", genre: "role play", game_console_id: 2)
+call_of_duty = Game.create(name: "Call of Duty", genre: "first person shooter", game_console_id: 2)
+tetris = Game.create(name: "Tetris", genre: "puzzle", game_console_id: 2)
+smash_bros = Game.create(name: "Smash Bros", genre: "fighting", game_console_id: 2)
+game_of_thrones = Game.create(name: "Game of Thrones", genre: "open world", game_console_id: 3)
+street fighter = Game.create(name: "Street Fighter", genre: "fighting", game_console_id: 3)
+fortnite = Game.create(name: "Fortnite", genre: "sandbox", game_console_id: 3)
+mortal_kombat = Game.create(name: "Mortal Kombat", genre: "fighting", game_console_id: 4)
+grand_theft_auto = Game.create(name: "Grand Theft Auto", genre: "open world", game_console_id: 4)
+minecraft = Game.create(name: "Minecraft", genre: "puzzle", game_console_id: 4)
+world_of_warcraft = Game.create(name: "World of Warcraft", genre: "role play", game_console_id: 4)
+
+
+
+
+
